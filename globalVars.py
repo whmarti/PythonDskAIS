a19b796c275c0b68d4f27ba8e62e34976e5e4347
Dev. William Martin <whmarti@hotmail.com>
2021-11-04 00:55:39 +1300

    Structuration

---
+++ globalVars.py
@@ -0,0 +1,47 @@
+#*************************************************************************
+# Develop of Courses Outline 
+# Auckland Institute of Studies
+# Developers: William Martin, June , Sun....
+# Date: 03/11/2021
+# File: Definition of Global variables
+#*************************************************************************
+#File variables
+global original,fileSize,target
+
+#Variables of position of controls --------------------------------------
+
+# Controls Margins:
+xPosL=50  #Labels
+xPosF=160 #Fields
+yPos=110  #Vertical
+yBtnPos=110  #Button Upload
+
+
+#Comboboxes:
+# ycbxPos=120
+xCbxLbl=120
+#-------------------------------------------------------------------------
+
+#Variables of controls ----------------------------------------------------
+
+# Titles:
+tFont='Arial'
+mtSize=18       # Main Title
+stSize=14       # Sub Title
+mtColor='Black' # Main Color
+stColor='Black' # Sub Color
+# Labels:
+lbFont='Arial'
+lbSize=10
+#SegmentLine
+bgSL='#5a7684'   # Seg Line Color
+#-------------------------------------------------------------------------
+
+#Variables of logic ----------------------------------------------------
+
+# Controls:
+years = ('2020', '2021', '2022', '2024')
+months = ('1', '2', '3')
+fieldsDocx = ["+Lecturer Name+","+E-mail address+"]
+
+#-------------------------------------------------------------------------
--- outlineUI.py
+++ outlineUI.py
@@ -6,219 +6,183 @@
 #*************************************************************************
 from tkinter.filedialog import askopenfile 
 from tkinter import *
-from tkinter.ttk import *
-
-import tkinter as tk,time
-from tkinter.constants import ANCHOR, NW, RIGHT, Y
+from tkinter.ttk import Progressbar,Button
 from PIL import ImageTk, Image
-import pandas as pd
-import docx
 from tkinter.messagebox import showinfo
-from tkinter import ttk 
-from tkinter import messagebox
+from tkinter import ttk , messagebox
+import tkinter as tk,time, pandas as pd
+import docx,shutil,os,globalVars as gv
 
-import shutil,os
+class Root(Tk):
+    def __init__(self):
+        super(Root, self).__init__()
+        self.title("Course Outline Generator")
+        self.geometry('700x500')
+        self.minsize(640,400)
 
+#<editor-fold desc="Functions">
+    def uploadFiles(self): 
+        if gv.original != "":
+            pb1 = Progressbar(
+                    root, 
+                    orient=HORIZONTAL, 
+                    length=300, 
+                    mode='determinate'
+                    )
+            pb1.place(x=gv.xPosF+120,y=gv.yBtnPos)
+            labelPorc = ttk.Label(root,font=("bold",8))
+            labelPorc.place(x=gv.xPosF+260,y=gv.yBtnPos+2)
+            for i in range(6):
+                root.update_idletasks()
+                pb1['value'] += 20
+                labelPorc.config(text=str(int(pb1['value'])-20) + "%")  
+                print(str(int(pb1['value'])-20))
+                time.sleep(0.1)
+            labelPorc.destroy()
+            pb1.destroy()
+            gv.target = 'docs'
+            try:
+                shutil.copy(gv.original, gv.target)
+                print("File copied successfully.")
+                messagebox.showinfo(title="INFORMATION", message='File Uploaded Successfully!')
+            # If source and destination are same
+            except shutil.SameFileError:
+                print("Source and destination represents the same file.")
 
+            # If there is any permission issue
+            except PermissionError:
+                print("Permission denied.")
 
-root = tk.Tk()
-root.configure(background='#FFFFFF')
-root.title("Course Outline Generator")
-root.geometry('700x500')
+            # For other errors
+            except:
+                print("Error occurred while copying file.")
+        else:
+            messagebox.showerror(title="INFORMATION", message='Insert the File!')
 
-logoImg = (Image.open("img/logo.jpg"))
-resizedImg = logoImg.resize((120,50), Image.ANTIALIAS)
-logoImg = ImageTk.PhotoImage(resizedImg)
-label = tk.Label(root, image=logoImg).place(x=280, y=10)
-heading = tk.Label(root, text="Course Outline Generator")
-heading.config(font=('Arial', 18))
-heading.pack(padx=50, pady=65)
+    def open_file(self):
+        file_path = askopenfile(mode='r', filetypes=[('Doc Files', '*docx')])
+        if file_path is not None:
+            gv.original=file_path.name
+            print (gv.original)
+            gv.fileSize = os.path.getsize(gv.original)
+            print('Size del archivo: ' + str(gv.fileSize))
+            self.uploadFiles()
+        else:
+            if gv.original != "":
+                messagebox.showerror(title="INFORMATION", message='No file chosen, Insert the file again!')
 
-#*************************************************************************
-# Here start the fields definition:
-ycbxPos=120
-xCbxLbl=108
+    def segmentLine(self):
+        w = tk.Canvas(self, width=690, height=3)
+        w.place(y=gv.yPos,x=0)
+        w.config( background=gv.bgSL)
 
-def trimester_changed(event):
-    msg = f'You selected {trimester_cb.get()}!'
-    showinfo(title='Result', message=msg)
-# Trimester of year
-months = ('1', '2', '3')
- #this creates 'Label' widget for Trimester.
-label_trimester =tk.Label(root,text="Trimester", width=20,font=("bold",10))
-label_trimester.place(x=xCbxLbl,y=ycbxPos) #x=48
-xCbxLbl+=172
+    def show_entry_fields(self):
+        # fieldsDocx = ["+Lecturer Name+","+E-mail address+"]
+        # valuesDocx = [entry_name.get(), entry_email.get()]
+        print("Trimestre: %s\nYear: %s" % (trimester_cb.get(), year_cb.get()))
+        # print("Name: %s\nEmail: %s" % (entry_name.get(), entry_email.get()))
+        document = docx.Document('docs/TempleteCO.docx')
+        # for par in document.paragraphs:  # to extract the whole text
+        #     for i in range(len(fieldsDocx)):
+        #         if gv.fieldsDocx[i] in par.text:
+        #             if len(valuesDocx[i])>0:
+        #                 tmp_text = par.text
+        #                 print(tmp_text)
+        #                 tmp_text = tmp_text.replace(fieldsDocx[i],valuesDocx[i])
+        #                 par.text=tmp_text
+        #                 print(tmp_text)
+        #                 break
+        # document.save('docs/TempleteCO.docx')
+#</editor-fold>
 
-selected_month = tk.StringVar()
-trimester_cb = ttk.Combobox(root, textvariable=selected_month,width=5)
-trimester_cb['values'] = months
-trimester_cb['state'] = 'readonly'  # normal
-# trimester_cb.bind('<<ComboboxSelected>>', trimester_changed)
-trimester_cb.current(1)
-# entry_trimester=tk.Entry(root)
-trimester_cb.place(x=xCbxLbl,y=ycbxPos) #x=220
-xCbxLbl+=58
+#<editor-fold desc="Constructors">
+    def createHeader(self):
+        heading = tk.Label(self, text="Course Outline Generator")
+        heading.config(font=(gv.tFont, gv.mtSize),fg=gv.stColor)
+        heading.pack(padx=50, pady=65)
+        uplHeading = tk.Label(root, text="Upload Course Descriptor")
+        uplHeading.config(font=(gv.tFont, gv.stSize),fg=gv.stColor)
+        uplHeading.place(x=gv.xPosL, y=gv.yPos)
+        gv.yPos+=30
+        upld = tk.Button(self,text='Upload a Course Descriptor' , width=30,bg="blue",fg='white',activebackground='#0052cc', activeforeground='#aaffaa',command = self.open_file )
+        upld.place(x=gv.xPosL,y=gv.yPos)
+        gv.yBtnPos = gv.yPos
+        gv.yPos += 30
+        label_upl = tk.Label(self,text="Accepted file types: .doc and .docx (2MB limit)", width=35,font=(gv.lbFont,gv.lbSize))
+        label_upl.place(x=gv.xPosL,y=gv.yPos)
+        gv.yPos += 30
+        self.segmentLine()
+        gv.yPos += 20
 
-def year_changed(event):
-    msg = f'You selected {year_cb.get()}!'
-    showinfo(title='Result', message=msg)
-# Years
-years = ('2020', '2021', '2022', '2024')
- #this creates 'Label' widget for Year.
-label_year =tk.Label(root,text="Year", width=20,font=("bold",10))
-label_year.place(x=xCbxLbl,y=ycbxPos)  #278
-xCbxLbl+=172
+    def createPeriod(self):
+        
+        label_trimester =tk.Label(self,text="Trimester", width=8,font=(gv.lbFont,gv.lbSize))
+        label_trimester.place(x=gv.xCbxLbl,y=gv.yPos)
+        gv.xCbxLbl+=100
+        selected_month = tk.StringVar()
+        self.trimester_cb = ttk.Combobox(self, textvariable=selected_month,width=10)
+        self.trimester_cb['values'] = gv.months
+        self.trimester_cb['state'] = 'readonly'
+        self.trimester_cb.current(1)
+        self.trimester_cb.place(x=gv.xCbxLbl,y=gv.yPos)
+        gv.xCbxLbl+=110
 
-selected_year = tk.StringVar()
-year_cb = ttk.Combobox(root, textvariable=selected_year,width=5)
-year_cb['values'] = years
-year_cb['state'] = 'readonly'  # normal
-# year_cb.bind('<<ComboboxSelected>>', year_changed)
-year_cb.current(1)
-# entry_year=tk.Entry(root)
-year_cb.place(x=xCbxLbl,y=ycbxPos) #x=450
+           
+        label_year =tk.Label(root,text="Year", width=9,font=(gv.lbFont,gv.lbSize))
+        label_year.place(x=gv.xCbxLbl,y=gv.yPos) 
+        gv.xCbxLbl+=100
+        selected_year = tk.StringVar()
+        year_cb = ttk.Combobox(self, textvariable=selected_year,width=10)
+        year_cb['values'] = gv.years
+        year_cb['state'] = 'readonly' 
+        year_cb.current(1)
+        year_cb.place(x=gv.xCbxLbl,y=gv.yPos)
+        gv.yPos+=33
+        self.segmentLine()
+        gv.yPos+=20
 
-w = tk.Canvas(root, width=1200, height=3)
-# w.create_rectangle(0, 0, 1900, 2, fill="gray", outline = 'gray')
-# w.pack()
-w.place(y=160)
+    def createLecturerInf(self):    
+        lectHeading = tk.Label(self, text="Lecturer Information")
+        lectHeading.config(font=(gv.tFont, gv.stSize),fg=gv.stColor)
+        lectHeading.place(x=gv.xPosL, y=gv.yPos)
+        gv.yPos+=30
+        var=tk.StringVar()
+        tk.Radiobutton(self,text="Lecturer",padx= 5, variable= var, value="Lecturer").place(x=gv.xPosL,y=gv.yPos)
+        tk.Radiobutton(self,text="Course Coordinator",padx= 20, variable= var, value="Course Coordinator").place(x=gv.xPosF,y=gv.yPos)
+        gv.yPos+=40
+        label_name =tk.Label(self,text="Full Name", width=8,font=(gv.lbFont,gv.lbSize))
+        label_name.place(x=gv.xPosL,y=gv.yPos)
 
-# Controls Margins:
-xPosL=80
-xPosF=260
-yPos=180
+        entry_name=tk.Entry(self)
+        entry_name.config(width=40)
+        entry_name.place(x=gv.xPosF,y=gv.yPos)
+        gv.yPos+=30
 
-lectHeading = tk.Label(root, text="Lecturer Information")
-lectHeading.config(font=('Arial', 14))
-lectHeading.place(x=50, y=yPos)
-yPos+=50
+        #this creates 'Label' widget for Email.
+        label_email =tk.Label(self,text="Email", width=5,font=(gv.lbFont,gv.lbSize))
+        label_email.place(x=gv.xPosL,y=gv.yPos)
+        entry_email=tk.Entry(self)
+        entry_email.config(width=40)
+        entry_email.place(x=gv.xPosF,y=gv.yPos)
+        gv.yPos+=35
+        self.segmentLine()
+        gv.yPos+=30
 
-#the variable 'var' mentioned here holds Integer Value, by deault 0
-var=tk.StringVar()
-#this creates 'Radio button' widget and uses place() method
-tk.Radiobutton(root,text="Lecturer",padx= 5, variable= var, value="Lecturer").place(x=xPosL,y=yPos)
-tk.Radiobutton(root,text="Course Coordinator",padx= 20, variable= var, value="Course Coordinator").place(x=xPosF,y=yPos)
-yPos+=35
+    def endForm(self):
+        tk.Button(self, text='Download a Course Outline' , width=30,bg="green",fg='white',activebackground='#0052cc', activeforeground='#aaffaa', command=self.show_entry_fields).place(x=gv.xPosF,y=gv.yPos)
+#</editor-fold>
 
-#this creates 'Label' widget for Fullname.
-label_name =tk.Label(root,text="Full Name", width=20,font=("bold",10))
-label_name.place(x=xPosL,y=yPos)
+if __name__ == '__main__':
+    root = Root()
+    logoImg = (Image.open("img/logo.jpg"))
+    resizedImg = logoImg.resize((120,50), Image.ANTIALIAS)
+    logoImg = ImageTk.PhotoImage(resizedImg)
+    label = tk.Label(root, image=logoImg).place(x=280, y=10)
 
-entry_name=tk.Entry(root)
-entry_name.place(x=xPosF,y=yPos)
-yPos+=30
+   #Built of UI:
+    root.createHeader()
+    root.createPeriod()
+    root.createLecturerInf()
+    root.endForm()
 
-#this creates 'Label' widget for Email.
-label_email =tk.Label(root,text="Email", width=20,font=("bold",10))
-label_email.place(x=xPosL,y=yPos)
-
-#this will accept the input string text from the user.
-entry_email=tk.Entry(root)
-entry_email.place(x=xPosF,y=yPos)
-yPos+=30
-#*************************************************************************
-# Here start the Upload file section:
-# Course Descriptor section:
-original=""
-fileSize=tk.IntVar()
-def open_file():
-    
-    file_path = askopenfile(mode='r', filetypes=[('Doc Files', '*docx')])
-    if file_path is not None:
-        global original,fileSize 
-        original = file_path.name
-        fileSize = os.path.getsize(original)
-        print('Size del archivo: ' + str(fileSize))
-    else:
-        if original != "":
-            messagebox.showerror(title="INFORMATION", message='No file chosen, Insert the file again!')
-        # pass
-
-def uploadFiles(): 
-    if original != "":
-        pb1 = Progressbar(
-                root, 
-                orient=HORIZONTAL, 
-                length=300, 
-                mode='determinate'
-                )
-        pb1.place(x=xPosF,y=yPos)
-        labelPorc = ttk.Label(root,font=("bold",8))
-        labelPorc.place(x=xPosF+146,y=yPos+2)
-        for i in range(6):
-            root.update_idletasks()
-            pb1['value'] += 20
-            labelPorc.config(text=str(int(pb1['value'])-20) + "%")  
-            print(str(int(pb1['value'])-20))
-            time.sleep(0.1)
-        labelPorc.destroy()
-        pb1.destroy()
-        target = 'docs'
-        try:
-            shutil.copy(original, target)
-            print("File copied successfully.")
-            messagebox.showinfo(title="INFORMATION", message='File Uploaded Successfully!')
-        # If source and destination are same
-        except shutil.SameFileError:
-            print("Source and destination represents the same file.")
-
-        # If there is any permission issue
-        except PermissionError:
-            print("Permission denied.")
-
-        # For other errors
-        except:
-            print("Error occurred while copying file.")
-    else:
-        messagebox.showerror(title="INFORMATION", message='Insert the File!')
-
-
-yPos+=30
-
-label_doc =tk.Label(root,text="Course Descriptor", width=20,font=("bold",10))
-label_doc.place(x=xPosL,y=yPos)
-
-adharbtn = Button(root, text ='Choose docx File', width=20,command = lambda:open_file()) 
-adharbtn.place(x=xPosF,y=yPos)
-
-yPos+=30
-
-upld = tk.Button(root,text='Upload a Course Descriptor' , width=30,bg="blue",fg='white',activebackground='#0052cc', activeforeground='#aaffaa',command=uploadFiles )
-upld.place(x=xPosF,y=yPos)
-
-yPos+=30
-
-#this creates button for submitting the details provides by the user
-def show_entry_fields():
-    fieldsDocx = ["+Lecturer Name+","+E-mail address+"]
-    valuesDocx = [entry_name.get(), entry_email.get()]
-    print("Trimestre: %s\nYear: %s" % (trimester_cb.get(), year_cb.get()))
-    print("Name: %s\nEmail: %s" % (entry_name.get(), entry_email.get()))
-    document = docx.Document('docs/TempleteCO.docx')
-    for par in document.paragraphs:  # to extract the whole text
-        for i in range(len(fieldsDocx)):
-            if fieldsDocx[i] in par.text:
-                if len(valuesDocx[i])>0:
-                    tmp_text = par.text
-                    print(tmp_text)
-                    tmp_text = tmp_text.replace(fieldsDocx[i],valuesDocx[i])   
-                    par.text=tmp_text
-                    print(tmp_text)
-                    break    
-    document.save('docs/TempleteCO.docx')
-
-tk.Button(root, text='Download a Course Outline' , width=30,bg="green",fg='white',activebackground='#0052cc', activeforeground='#aaffaa', command=show_entry_fields).place(x=xPosF,y=yPos)
-
-
-#*************************************************************************
-# Here start the handle of the document:
-# Searching of fields inside:\
-def replace_fields():
-    doc = docx.Document('docs/TempleteCO.docx')
-
-
-
-root.mainloop()
-
-
-
+    root.mainloop()
