c3b43d207c155db2154931d025304daedbbed194
Dev. William Martin <whmarti@hotmail.com>
2021-11-03 13:41:16 +1300

    Test rule 1

--- outlineUI.py
+++ outlineUI.py
@@ -1,5 +1,5 @@
 #*************************************************************************
-# Develop of Courses Outline
+# Develop of Courses Outline Hola Developer 1
 # Auckland Institute of Studies
 # Developers: William Martin, June , Sun....
 # Date of creation 03/11/2021
